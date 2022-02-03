49d90bc61356eea12c7e33c47f86a274ff206a64
karina-kasimova <casimowa.c2001@gmail.com>
2022-02-03 22:26:40 +0300

    first push

---
+++ public/js/modal.js
@@ -0,0 +1,15 @@
+document.getElementById('button_modal').addEventListener('click', function() {
+    document.querySelector('.bg-modal').style.display = 'flex';
+})
+
+document.querySelector('.close').addEventListener('click', function() {
+    document.querySelector('.bg-modal').style.display = 'none';
+})
+
+document.getElementById('button_modal2').addEventListener('click', function() {
+    document.querySelector('.bg-modal').style.display = 'flex';
+})
+
+document.querySelector('.close').addEventListener('click', function() {
+    document.querySelector('.bg-modal').style.display = 'none';
+})
